539df2ddd0b92cd3f0b5daa563bdab9c4385182f
Artur Czocher <artur.czocher@wasper.pl>
2017-01-24 21:42:13 +0100

    Activity and menu skeleton

--- app/src/main/java/pl/wasper/popularmovies/ListActivity.java
+++ app/src/main/java/pl/wasper/popularmovies/ListActivity.java
@@ -2,12 +2,37 @@ package pl.wasper.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ListActivity extends AppCompatActivity {
+
+    private static final String API_KEY = "f21dbce7bd7679a7e0c3054ecb21fd95";
+//    private static final String HOST =
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.top_rated_item:
+                break;
+            case R.id.most_popular_item:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
